edd2c36c9df96557b0fe79444541c8d21e16e2b4
Kaylee de Soto <kdesoto@g.harvard.edu>
2025-01-20 19:45:36 -0500

    REmoved unnecessary function

--- src/decat_obsplan_generator/airmass.py
+++ src/decat_obsplan_generator/airmass.py
@@ -2,22 +2,22 @@
 import os
 from pathlib import Path
 
+import astropy.units as u
+import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
 from astropy.coordinates import SkyCoord
 from astropy.time import Time
-import astropy.units as u
-import matplotlib.dates as mdates
 
 from .helpers import ctio_location
+
 
 class AirmassCalculator:
     """Class that performs airmass calculations and caches
     for later access."""
 
     def __init__(self, location=None):
-        """Initializes location airmass is calculated from.
-        """
+        """Initializes location airmass is calculated from."""
         if location is None:
             self._loc = ctio_location()
         else:
@@ -27,57 +27,40 @@ class AirmassCalculator:
         pref_b = self._loc.temperature.value
         pref_c = self._loc.relative_humidity
         file_prefix = f"{pref_a}_{pref_b}_{pref_c}"
-        cache_dir = os.path.join(
-            Path(__file__).parent.parent.parent.absolute(),
-            f"data/airmass_grids"
-        )
+        cache_dir = os.path.join(Path(__file__).parent.parent.parent.absolute(), "data/airmass_grids")
         os.makedirs(cache_dir, exist_ok=True)
         self._cache_fn = os.path.join(cache_dir, f"{file_prefix}.feather")
 
         # hour angle / declination grid
         # here we only go to HA = 5:15:00 because that's where DECam cuts off
-        ha_deg_range = np.linspace(0., 78.75, num = 1_000) # symmetric so only positives
-        dec_range = np.linspace(-90.0, 40.0, num = 1_000) # above 40deg not visible
+        ha_deg_range = np.linspace(0.0, 78.75, num=1_000)  # symmetric so only positives
+        dec_range = np.linspace(-90.0, 40.0, num=1_000)  # above 40deg not visible
         _ha_grid, _dec_grid = np.meshgrid(ha_deg_range, dec_range)
         self._ha_grid = _ha_grid.ravel()
         self._dec_grid = _dec_grid.ravel()
 
-
-    def calculate_airmass(self, times, coordinate: SkyCoord):
-        """Return airmass value at specific times
-        for single coordinate."""
-        return self._loc.altaz(times, target=coordinate).secz
-
-
     def generate_airmass_grid(self):
-        """From grid of hour angles and declinations, generate
-        """
-        dummy_time = Time.now() # just to convert from HA to RA and back
+        """From grid of hour angles and declinations, generate"""
+        dummy_time = Time.now()  # just to convert from HA to RA and back
 
         ra_values = (self._loc.local_sidereal_time(dummy_time).degree - self._ha_grid) % 360
-        sky_coords = SkyCoord(ra=ra_values * u.deg, dec=self._dec_grid * u.deg, frame='icrs')
+        sky_coords = SkyCoord(ra=ra_values * u.deg, dec=self._dec_grid * u.deg, frame="icrs")
 
         # Convert to AltAz frame (to then retrieve airmass)
         airmasses = self._loc.altaz(time=dummy_time, target=sky_coords).secz
-        airmasses[airmasses < 0.0] = np.nan # remove invalid airmasses
+        airmasses[airmasses < 0.0] = np.nan  # remove invalid airmasses
 
         self._cache = pd.DataFrame(
-            np.array([self._ha_grid, self._dec_grid, airmasses]).T,
-            columns=['ha', 'dec', 'airmass']
+            np.array([self._ha_grid, self._dec_grid, airmasses]).T, columns=["ha", "dec", "airmass"]
         )
         self._cache.to_feather(self._cache_fn)
 
-
     def load_airmass_grid(self):
-        """Load and return airmass grid.
-        """
+        """Load and return airmass grid."""
         if not os.path.exists(self._cache_fn):
-            raise FileNotFoundError(
-                "Cache not yet created! Please run self.generate_airmass_grid() first."
-            )
+            raise FileNotFoundError("Cache not yet created! Please run self.generate_airmass_grid() first.")
         self._cache = pd.read_feather(self._cache_fn)
         return self._cache.copy()
-
 
     def query_airmass(self, ra, dec, times):
         """From a RA/dec and set of times,
@@ -88,19 +71,17 @@ class AirmassCalculator:
 
         return self._query_from_ha_dec(hour_angles, dec.degree)
 
-
     def _query_from_ha_dec(self, ha, dec):
-        """Query cache from hour angle(s) and SINGLE declination.
-        """
+        """Query cache from hour angle(s) and SINGLE declination."""
         if not np.isscalar(dec):
             raise TypeError("dec must be a scalar")
 
-        if dec >= 40.0: # out of bounds
+        if dec >= 40.0:  # out of bounds
             return np.nan
 
         # take absolute values, airmasses symmetric around HA
         abs_ha = np.atleast_1d(np.abs(ha))
-        oob_mask = (abs_ha >= 78.75) # out of bounds for DECam
+        oob_mask = abs_ha >= 78.75  # out of bounds for DECam
 
         # 0.1 deg dec mask, grid is dense enough for this
         dec_df = self._cache.loc[(self._cache.dec - dec).abs() < 0.1]
@@ -108,25 +89,20 @@ class AirmassCalculator:
         airmasses = []
         for hour_angle in abs_ha[~oob_mask]:
             candidate_df = dec_df.loc[(dec_df.ha - hour_angle).abs() < 0.1]
-            sep_sq = self._separation_squared(
-                candidate_df.ha, candidate_df.dec,
-                hour_angle, dec
-            )
-            airmasses.append(candidate_df.loc[sep_sq.idxmin(), 'airmass'])
+            sep_sq = self._separation_squared(candidate_df.ha, candidate_df.dec, hour_angle, dec)
+            airmasses.append(candidate_df.loc[sep_sq.idxmin(), "airmass"])
 
         airmasses_full = np.nan * np.ones(len(abs_ha))
         airmasses_full[~oob_mask] = airmasses
 
         return airmasses_full
-            
 
     def _separation_squared(self, ra_arr, dec_arr, ra2, dec2):
         """Helper function to calculate separation
         directly from floats in degrees. Assumes
         small angle approximation.
         """
-        return ((ra_arr - ra2)*np.cos((dec_arr+dec2) * np.pi/360.))**2+(dec_arr - dec2)**2
-
+        return ((ra_arr - ra2) * np.cos((dec_arr + dec2) * np.pi / 360.0)) ** 2 + (dec_arr - dec2) ** 2
 
     def plot_airmass(self, fig, ax, ra, dec, times=None, **plot_kwargs):
         """Add airmass plot to ax, assuming
@@ -146,24 +122,12 @@ class AirmassCalculator:
         ax.plot(time_arr.to_datetime(), airmasses, **plot_kwargs)
 
         # Formatting x-axis ticks to the desired format
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
         ax.invert_yaxis()
         ax.set_ylabel("Airmass")
-        ax.axhline(1.8, linestyle='dashed', color='grey')
+        ax.axhline(1.8, linestyle="dashed", color="grey")
 
         # Auto-format the x-axis for better readability
         fig.autofmt_xdate()
 
         return fig, ax
-
-
-
-
-
-
-
-
-
-
-
-    
--- src/decat_obsplan_generator/helpers.py
+++ src/decat_obsplan_generator/helpers.py
@@ -1,15 +1,19 @@
-from astroplan import Observer
 import astropy.units as u
+from astroplan import Observer
+
 
 def ctio_location(
-    temperature = 5. * u.deg_C,
-    pressure = 780. * u.mbar,
-    relative_humidity = 0.5,
+    temperature=5.0 * u.deg_C,
+    pressure=780.0 * u.mbar,
+    relative_humidity=0.5,
 ):
     """Generate Observer (which inherits from EarthLocation)
     for CTIO.
     """
     return Observer.at_site(
-        "CTIO", timezone="Etc/GMT+3", pressure=pressure,
-        temperature=temperature, relative_humidity=relative_humidity
+        "CTIO",
+        timezone="Etc/GMT+3",
+        pressure=pressure,
+        temperature=temperature,
+        relative_humidity=relative_humidity,
     )
